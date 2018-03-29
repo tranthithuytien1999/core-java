a52eb2bd0b5acb42efe19923fbf4c9a8017556ed
tranthithuytien1999 <37666517+tranthithuytien1999@users.noreply.github.com>
2018-03-29 22:51:11 +0700

    Add files via upload

---
+++ src/java53/bai3.java
@@ -0,0 +1,15 @@
+package java53;
+
+public class bai3 {
+	public static void main(String[] args) {   
+		 int [][]a = new int[10][10];    
+		 for(int i = 0; i < 10; i++)
+		   {
+		      for(int j = 0; j < 10; j++)
+		      {
+		         System.out.printf("%2d ", a[i][j]);
+		      }
+		      System.out.println();
+		   }
+		}
+}
